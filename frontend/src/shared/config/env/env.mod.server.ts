cb7b12e33ff8d2cb611904cc4bba6055ff273fc3
Bryan Mendoza <bmendoza.dev@hotmail.com>
2026-06-15 03:33:21 -0600

    fix(frontend): allow any BACKEND_URL protocol in production

--- frontend/src/shared/config/env/env.mod.server.ts
+++ frontend/src/shared/config/env/env.mod.server.ts
@@ -2,9 +2,11 @@ import * as z from "zod";
 
 import { createURLSchema } from "#/shared/constants/schemas/schemas.mod.server";
 
+const IS_PRODUCTION = process.env["NODE_ENV"] === "production";
+
 export const envSchema = z.object({
   BACKEND_URL: createURLSchema({
-    protocol: /^http$/,
+    protocol: IS_PRODUCTION ? undefined : /^http$/,
   }),
   DATABASE_URL: z.string().trim().nonempty(),
 });
