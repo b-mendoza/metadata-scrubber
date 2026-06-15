16a3689e0b879f6a8ec69b4e07d42d55cd2238b5
Bryan Mendoza <bmendoza.dev@hotmail.com>
2026-06-15 03:18:00 -0600

    refactor(frontend): list BACKEND_URL before DATABASE_URL in env schema

--- frontend/src/shared/config/env/env.mod.server.ts
+++ frontend/src/shared/config/env/env.mod.server.ts
@@ -3,10 +3,10 @@ import * as z from "zod";
 import { createURLSchema } from "#/shared/constants/schemas/schemas.mod.server";
 
 export const envSchema = z.object({
-  DATABASE_URL: z.string().trim().nonempty(),
   BACKEND_URL: createURLSchema({
     protocol: /^http$/,
   }),
+  DATABASE_URL: z.string().trim().nonempty(),
 });
 
 export interface Env extends z.output<typeof envSchema> {}
