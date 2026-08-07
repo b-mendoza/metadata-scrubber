69ee2bc29940fb7998b388ad4a044b13cd5fe262
Bryan Mendoza <bmendoza.dev@hotmail.com>
2026-08-07 03:03:46 -0600

    feat(scrub): add CleanPDF write and verification helpers

---
+++ backend/internal/scrub/write.go
@@ -0,0 +1,43 @@
+package scrub
+
+import (
+	"errors"
+
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
+)
+
+func verifyScrubbedPDF(outputBytes []byte) error {
+	verificationContext, err := readPDF(outputBytes)
+	if err != nil {
+		return err
+	}
+	verificationAnalysis, err := analyzePDF(verificationContext, PostWriteVerification)
+	if err != nil {
+		return err
+	}
+	if len(verificationAnalysis.fields) != 0 {
+		return errors.New("PDF metadata remained after scrub")
+	}
+	return nil
+}
+
+func removeAnalyzedMetadata(context *model.Context, analysis *pdfAnalysis) {
+	for _, target := range analysis.infoTargets {
+		delete(target.dictionary, target.key)
+	}
+	for _, target := range analysis.metadataTargets {
+		delete(target.dictionary, target.key)
+	}
+
+	context.Title = ""
+	context.Subject = ""
+	context.Author = ""
+	context.Creator = ""
+	context.Producer = ""
+	context.XRefTable.CreationDate = ""
+	context.ModDate = ""
+	context.Keywords = ""
+	context.KeywordList = map[string]bool{}
+	context.Properties = map[string]string{}
+	context.CatalogXMPMeta = nil
+}
