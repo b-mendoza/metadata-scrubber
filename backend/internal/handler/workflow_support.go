e816800c68ec033d32787d227bd680e4dfbe390d
Bryan Mendoza <bmendoza.dev@hotmail.com>
2026-08-10 18:21:44 -0600

    refactor(backend): format file IDs with fmt.Sprintf

--- backend/internal/handler/workflow_support.go
+++ backend/internal/handler/workflow_support.go
@@ -2,9 +2,9 @@ package handler
 
 import (
 	"context"
-	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"mime"
@@ -31,17 +31,8 @@ func (handler *Handler) newFileID() (string, bool) {
 	uuidBytes[6] = (uuidBytes[6] & 0x0f) | 0x40
 	uuidBytes[8] = (uuidBytes[8] & 0x3f) | 0x80
 
-	encoded := make([]byte, 36)
-	hex.Encode(encoded[0:8], uuidBytes[0:4])
-	encoded[8] = '-'
-	hex.Encode(encoded[9:13], uuidBytes[4:6])
-	encoded[13] = '-'
-	hex.Encode(encoded[14:18], uuidBytes[6:8])
-	encoded[18] = '-'
-	hex.Encode(encoded[19:23], uuidBytes[8:10])
-	encoded[23] = '-'
-	hex.Encode(encoded[24:36], uuidBytes[10:16])
-	return string(encoded), true
+	return fmt.Sprintf("%x-%x-%x-%x-%x",
+		uuidBytes[0:4], uuidBytes[4:6], uuidBytes[6:8], uuidBytes[8:10], uuidBytes[10:16]), true
 }
 
 func decodeJSONRequest(w http.ResponseWriter, request *http.Request, destination any) bool {
